11ae8b7427c379961bed0afcc1b5678f329ac53f
Carlos Jose Sanchez <carlosjosesancheze@gmail.com>
2026-08-19 15:25:00 -0400

    widget

--- src/components/aurora-widget.tsx
+++ src/components/aurora-widget.tsx
@@ -6,10 +6,10 @@ export default function AuroraWidget() {
   useEffect(() => {
     const script = document.createElement("script");
 
-    script.src = "https://staging.qchat.qodari.com/widget.js";
+    script.src = "https://app.linkaurora.com/widget.js";
     script.setAttribute(
       "data-assistant",
-      "0dcccb54-fc31-497c-80f5-04fb0b3c793d",
+      "86d196e1-ae0f-48b5-87cf-1a0c75e7195b",
     );
 
     script.async = true;
